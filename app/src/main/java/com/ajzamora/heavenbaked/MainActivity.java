c1fcd4cd605916bc41aa6b35db14a8cab38617b8
zam <ajmzamora@gmail.com>
2020-05-24 02:08:03 -0400

    set main activity ui

--- app/src/main/java/com/ajzamora/heavenbaked/MainActivity.java
+++ app/src/main/java/com/ajzamora/heavenbaked/MainActivity.java
@@ -1,14 +1,33 @@
 package com.ajzamora.heavenbaked;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+
+import com.ajzamora.heavenbaked.databinding.ActivityMainBinding;
+import com.ajzamora.heavenbaked.utils.LayoutUtils;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding mMainBinding;
+    RecipeAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        mMainBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(mMainBinding.getRoot());
+        initUI();
     }
+
+    private void initUI() {
+        final int COLUMN_WIDTH = getResources().getInteger(R.integer.rv_recipe_column_widthDp);
+        final int COLUMN_NUM = LayoutUtils.calculateNoOfColumns(this, COLUMN_WIDTH);
+        mAdapter = new RecipeAdapter();
+        mMainBinding.rvRecipeMain.setAdapter(mAdapter);
+        mMainBinding.rvRecipeMain.setLayoutManager(new GridLayoutManager(this, COLUMN_NUM));
+        mMainBinding.rvRecipeMain.setHasFixedSize(true);
+    }
+
 }
