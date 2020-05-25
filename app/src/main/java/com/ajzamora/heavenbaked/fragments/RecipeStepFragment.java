1fd7a8cabb036977ea68732ca89bd7ff4ef012cd
zam <ajmzamora@gmail.com>
2020-05-25 02:03:45 -0400

    add ExoPlayer event listeners

--- app/src/main/java/com/ajzamora/heavenbaked/fragments/RecipeStepFragment.java
+++ app/src/main/java/com/ajzamora/heavenbaked/fragments/RecipeStepFragment.java
@@ -2,6 +2,7 @@ package com.ajzamora.heavenbaked.fragments;
 
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,18 +14,26 @@ import androidx.fragment.app.Fragment;
 import com.ajzamora.heavenbaked.R;
 import com.ajzamora.heavenbaked.databinding.FragRecipeStepBinding;
 import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
 import com.google.android.exoplayer2.ExoPlayerFactory;
 import com.google.android.exoplayer2.LoadControl;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.Player;
 import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
 import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
 import com.google.android.exoplayer2.source.ExtractorMediaSource;
 import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
 import com.google.android.exoplayer2.trackselection.TrackSelector;
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
 import com.google.android.exoplayer2.util.Util;
 
-public class RecipeStepFragment extends Fragment {
+public class RecipeStepFragment extends Fragment implements ExoPlayer.EventListener {
+    private static final String LOG_TAG = RecipeStepFragment.class.getSimpleName();
     public static final String EXTRA_STEP = "extra_step";
 
     FragRecipeStepBinding mFragRecipeStepBinding;
@@ -51,6 +60,8 @@ public class RecipeStepFragment extends Fragment {
             LoadControl loadControl = new DefaultLoadControl();
             mExoPlayer = ExoPlayerFactory.newSimpleInstance(getContext(), trackSelector, loadControl);
             mFragRecipeStepBinding.exoPlayerView.setPlayer(mExoPlayer);
+            // Set the ExoPlayer.EventListener to this activity
+            mExoPlayer.addListener(this);
             // Prepare the MediaSource.
             String userAgent = Util.getUserAgent(getContext(), getContext().getString(R.string.app_name));
             MediaSource mediaSource = new ExtractorMediaSource(mediaUri, new DefaultDataSourceFactory(
@@ -70,5 +81,59 @@ public class RecipeStepFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
         releasePlayer();
+    }
+
+    @Override
+    public void onTimelineChanged(Timeline timeline, Object manifest, int reason) {
+
+    }
+
+    @Override
+    public void onTracksChanged(TrackGroupArray trackGroups, TrackSelectionArray trackSelections) {
+
+    }
+
+    @Override
+    public void onLoadingChanged(boolean isLoading) {
+
+    }
+
+    @Override
+    public void onPlayerStateChanged(boolean playWhenReady, int playbackState) {
+        if((playbackState == Player.STATE_READY) && playWhenReady){
+            Log.d(LOG_TAG, "onPlayerStateChanged: PLAYING");
+        } else if((playbackState == Player.STATE_READY)){
+            Log.d(LOG_TAG, "onPlayerStateChanged: PAUSED");
+        }
+    }
+
+    @Override
+    public void onRepeatModeChanged(int repeatMode) {
+
+    }
+
+    @Override
+    public void onShuffleModeEnabledChanged(boolean shuffleModeEnabled) {
+
+    }
+
+    @Override
+    public void onPlayerError(ExoPlaybackException error) {
+
+    }
+
+    @Override
+    public void onPositionDiscontinuity(int reason) {
+
+    }
+
+    @Override
+    public void onPlaybackParametersChanged(PlaybackParameters playbackParameters) {
+
+    }
+
+    @Override
+    public void onSeekProcessed() {
+
     }
 }
