12668244753c898820d3713a7d408b6108e01854
zam <ajmzamora@gmail.com>
2020-05-26 02:02:11 -0400

    add two pane layout for sw600 Master/Detail flow

--- app/src/main/java/com/ajzamora/heavenbaked/fragments/RecipeDetailFragment.java
+++ app/src/main/java/com/ajzamora/heavenbaked/fragments/RecipeDetailFragment.java
@@ -9,15 +9,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import com.ajzamora.heavenbaked.R;
 import com.ajzamora.heavenbaked.adapters.RecipeDetailAdapter;
 import com.ajzamora.heavenbaked.databinding.FragRecipeDetailBinding;
 import com.ajzamora.heavenbaked.interfaces.IRecyclerItemClickListener;
 import com.ajzamora.heavenbaked.ui.DetailActivity;
 import com.ajzamora.heavenbaked.ui.StepActivity;
-import com.ajzamora.heavenbaked.utils.LayoutUtils;
 
 public class RecipeDetailFragment extends Fragment implements IRecyclerItemClickListener {
     private String mRecipeDetail;
@@ -29,13 +28,10 @@ public class RecipeDetailFragment extends Fragment implements IRecyclerItemClick
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mRecipeDetail = getActivity().getIntent().getStringExtra(DetailActivity.EXTRA_RECIPE);
-
-        final int COLUMN_WIDTH = getResources().getInteger(R.integer.rv_recipe_column_widthDp);
-        final int COLUMN_NUM = LayoutUtils.calculateNoOfColumns(getContext(), COLUMN_WIDTH);
         FragRecipeDetailBinding fragRecipeDetailBinding = FragRecipeDetailBinding.inflate(LayoutInflater.from(getContext()), container, false);
         RecipeDetailAdapter recipeDetailAdapter = new RecipeDetailAdapter(mRecipeDetail, this);
         fragRecipeDetailBinding.rvRecipeDetail.setAdapter(recipeDetailAdapter);
-        fragRecipeDetailBinding.rvRecipeDetail.setLayoutManager(new GridLayoutManager(getContext(), COLUMN_NUM));
+        fragRecipeDetailBinding.rvRecipeDetail.setLayoutManager(new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false));
         fragRecipeDetailBinding.rvRecipeDetail.setHasFixedSize(true);
         return fragRecipeDetailBinding.getRoot();
     }
--- app/src/main/java/com/ajzamora/heavenbaked/ui/DetailActivity.java
+++ app/src/main/java/com/ajzamora/heavenbaked/ui/DetailActivity.java
@@ -3,15 +3,32 @@ package com.ajzamora.heavenbaked.ui;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.ajzamora.heavenbaked.R;
+import com.ajzamora.heavenbaked.fragments.RecipeStepFragment;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_RECIPE = "extra_recipe";
+    private boolean mTwoPane;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        if (findViewById(R.id.fr_step_container) != null) {
+            mTwoPane = true;
+            if (savedInstanceState == null) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                RecipeStepFragment recipeStepFragment = new RecipeStepFragment();
+                fragmentManager.beginTransaction()
+                        .add(R.id.fr_step_container, recipeStepFragment)
+                        .commit();
+            }
+
+        } else {
+            mTwoPane = false;
+        }
     }
 }
