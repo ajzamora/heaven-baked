7af6ecb020428a0b0034749be15bd7809b2f7aeb
zam <ajmzamora@gmail.com>
2020-05-28 15:47:41 -0400

    use a single hierarchical dao
    delete unnecessary dao

--- app/src/main/java/com/ajzamora/heavenbaked/data/dao/RecipeDao.java
+++ app/src/main/java/com/ajzamora/heavenbaked/data/dao/RecipeDao.java
@@ -5,29 +5,16 @@ import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
-import androidx.room.Transaction;
 import androidx.room.Update;
 
 import com.ajzamora.heavenbaked.data.entity.Recipe;
-import com.ajzamora.heavenbaked.data.entity.RecipeIngredientCrossRef;
-import com.ajzamora.heavenbaked.data.relations.RecipeWithIngredients;
-import com.ajzamora.heavenbaked.data.relations.RecipeWithIngredientsAndMeasure;
 
 import java.util.List;
 
 @Dao
 public interface RecipeDao {
-    @Transaction
     @Query("SELECT * FROM recipe")
-    public List<RecipeWithIngredients> getRecipesWithIngredients();
-
-    @Transaction
-    @Query("SELECT * FROM Measure")
-    public List<RecipeWithIngredientsAndMeasure> getRecipesWithIngredientsAndMeasure();
-
-
-    @Query("SELECT * FROM recipe")
-    List<Recipe> loadAllTasks();
+    List<Recipe> getRecipeList();
 
     @Insert
     void insertRecipe(Recipe recipe);
