5fb387af2b5cdd3dc2f20ddccd7e3b975f1668b1
zam <ajmzamora@gmail.com>
2020-05-29 16:39:19 -0400

    implement parcelable on class step

--- app/src/main/java/com/ajzamora/heavenbaked/data/Step.java
+++ app/src/main/java/com/ajzamora/heavenbaked/data/Step.java
@@ -1,5 +1,8 @@
 package com.ajzamora.heavenbaked.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -9,7 +12,7 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 
 import java.io.Serializable;
 
-public class Step implements Serializable {
+public class Step implements Serializable, Parcelable {
 
     @SerializedName("id")
     @Expose
@@ -112,4 +115,38 @@ public class Step implements Serializable {
         return new EqualsBuilder().append(description, rhs.description).append(id, rhs.id).append(shortDescription, rhs.shortDescription).append(videoURL, rhs.videoURL).append(thumbnailURL, rhs.thumbnailURL).isEquals();
     }
 
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(this.id);
+        dest.writeString(this.shortDescription);
+        dest.writeString(this.description);
+        dest.writeString(this.videoURL);
+        dest.writeString(this.thumbnailURL);
+    }
+
+    protected Step(Parcel in) {
+        this.id = in.readInt();
+        this.shortDescription = in.readString();
+        this.description = in.readString();
+        this.videoURL = in.readString();
+        this.thumbnailURL = in.readString();
+    }
+
+    public static final Parcelable.Creator<Step> CREATOR = new Parcelable.Creator<Step>() {
+        @Override
+        public Step createFromParcel(Parcel source) {
+            return new Step(source);
+        }
+
+        @Override
+        public Step[] newArray(int size) {
+            return new Step[size];
+        }
+    };
 }
