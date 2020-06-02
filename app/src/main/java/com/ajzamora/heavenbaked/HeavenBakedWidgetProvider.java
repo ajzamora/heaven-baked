aa5413d0ee4bedf0904c71ea256bf367021a65c0
Unknown <unknown@example.com>
2020-06-03 02:15:06 +0800

    show last viewed recipe + ingredients in the widget

--- app/src/main/java/com/ajzamora/heavenbaked/HeavenBakedWidgetProvider.java
+++ app/src/main/java/com/ajzamora/heavenbaked/HeavenBakedWidgetProvider.java
@@ -5,34 +5,66 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Parcelable;
+import android.text.method.ScrollingMovementMethod;
 import android.widget.RemoteViews;
 
-import com.ajzamora.heavenbaked.ui.MainActivity;
+import com.ajzamora.heavenbaked.data.Ingredient;
+import com.ajzamora.heavenbaked.data.entity.Recipe;
+import com.ajzamora.heavenbaked.ui.DetailActivity;
+import com.ajzamora.heavenbaked.utils.FormatUtils;
+
+import java.util.List;
 
 /**
  * Implementation of App Widget functionality.
  */
 public class HeavenBakedWidgetProvider extends AppWidgetProvider {
 
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager, Recipe recipe,
                                 int appWidgetId) {
 
-        // Create an Intent to launch MainActivity when clicked
-        Intent intent = new Intent(context, MainActivity.class);
+        // Create an Intent to launch DetailActivity when clicked
+        Intent intent = new Intent(context, DetailActivity.class);
+        intent.putExtra(DetailActivity.EXTRA_RECIPE, (Parcelable) recipe);
         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.heaven_baked_widget);
+        views.setTextViewText(R.id.tv_recipe_label_widget, recipe.getName());
+        views.setTextViewText(R.id.tv_ingredients_widget, formatIngredientList(recipe.getIngredients()));
         // Widgets allow click handlers to only launch pending intents
-        views.setOnClickPendingIntent(R.id.widget_cupcake_image, pendingIntent);
+        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
+    private static String formatIngredientList(List<Ingredient> ingredients) {
+        final char SEPARATOR = ' ';
+        final String HYPHEN_IN_SPACES = " - ";
+        StringBuilder formattedIngredientListBuilder = new StringBuilder();
+        for (Ingredient ingredient : ingredients) {
+
+            formattedIngredientListBuilder.append(FormatUtils.toFormattedStringDecimal(ingredient.getQuantity(), 2));
+
+            formattedIngredientListBuilder.append(SEPARATOR)
+                    .append(ingredient.getMeasure());
+
+            formattedIngredientListBuilder.append(HYPHEN_IN_SPACES).append(ingredient.getIngredient());
+            formattedIngredientListBuilder.append("\n");
+        }
+
+        return formattedIngredientListBuilder.toString();
+    }
+
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
+//        IngredientsListingService.startListingIngredients(context);
+    }
+
+    public static void updateRecipeWidgets(Context context, AppWidgetManager appWidgetManager,
+                                           Recipe recipe, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+            updateAppWidget(context, appWidgetManager, recipe, appWidgetId);
         }
     }
 
